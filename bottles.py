2b09ca1c4041d27ce6425d436a99c8c833bfe1ad
Stephen-Exeter <st1018@exeter.ac.uk>
2025-09-30 16:55:31 +0100

    for loop

--- bottles.py
+++ bottles.py
@@ -1,10 +1,8 @@
-bottles = 0
 
-while bottles + 3 <= 10:
+
+for bottles in range(0,9,3):
     print(bottles, "green bottles hanging on the wall,")
     print(bottles, "green bottles hanging on the wall,")
     print("and if three green bottle should accidentally get hung up,")
     print("they'll be", bottles + 3, "green bottles hanging on the wall.")
     print()
-    bottles = bottles +3
-
