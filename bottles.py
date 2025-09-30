bc30cb77c900546ad63f1e84d0c3afeedf90a087
Stephen-Exeter <st1018@exeter.ac.uk>
2025-09-30 16:37:05 +0100

    count up to 10 bottles, start from 0

--- bottles.py
+++ bottles.py
@@ -1,12 +1,10 @@
-bottles = 20
+bottles = 0
 
-while bottles > 0:
+while bottles + 3 <= 10:
     print(bottles, "green bottles hanging on the wall,")
     print(bottles, "green bottles hanging on the wall,")
-    print("and if one green bottle should accidentally fall,")
-    print("they'll be", bottles-1, "green bottles hanging on the wall.")
+    print("and if three green bottle should accidentally get hung up,")
+    print("they'll be", bottles + 3, "green bottles hanging on the wall.")
     print()
-    bottles = bottles - 1
+    bottles = bottles +3
 
-
-print('All gone')
